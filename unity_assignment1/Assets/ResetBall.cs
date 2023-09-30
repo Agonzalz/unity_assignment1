f8f11ffcf83b81660d932b845d0cbfab6d40d25d
Agonzalz <60372524+Agonzalz@users.noreply.github.com>
2023-09-29 22:17:45 -0700

    scripts added

---
+++ unity_assignment1/Assets/ResetBall.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetBall : MonoBehaviour
+{
+    [SerializeField] Vector2 startposition;
+    //amount of frames that the ball has stopped
+    float timestopped = 0;
+    Rigidbody2D rigidb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidb = GetComponent<Rigidbody2D>();
+        startposition = (Vector2)transform.position;
+    }
+
+    //check if ball has stopped for a certain amount of frames
+    bool Stopped() {
+      if (rigidb.velocity.magnitude <= 0.1f){
+        timestopped += 1f;
+        return true;
+      }else {
+        return false;
+      }
+   }   
+
+   void OnBecameInvisible()
+   {
+    transform.position = startposition;
+    timestopped = 0;
+   }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Stopped() && timestopped == 1000f) {
+           rigidb.velocity = new Vector2(0, 0);
+           transform.position = startposition;
+           timestopped = 0;
+        }
+    }
+}
---
+++ unity_assignment1/Assets/playershot.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+public class playershot : MonoBehaviour
+{
+   [SerializeField] Rigidbody2D rigidb;
+   [SerializeField] LineRenderer line;
+   [SerializeField] float maxvelocity = 10f; 
+   [SerializeField] float velocity = 1f;
+   [SerializeField] Vector2 startposition;
+   bool dragging; 
+
+   void Drag() {
+      dragging = true;
+
+    }
+   void Dragmove() {
+
+   }
+   void Release(Vector2 endposition) {
+      dragging = false; 
+      //distance from ball to end of mouse input
+      float dist = Vector2.Distance(transform.position, endposition);
+
+      Vector2 direction = (Vector2)transform.position - endposition;
+      rigidb.velocity = Vector2.ClampMagnitude(direction * velocity, maxvelocity);
+   } 
+
+   //check if ball is stopped
+   bool Stopped() {
+      if (rigidb.velocity.magnitude == 0f){
+         return true;
+      } else {
+         return false;
+      }
+   }   
+
+      //handles player input
+      void input (){
+         //get mouse postion on input 
+         Vector2 inputpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+         //distance of line drawn from position of ball and mouse input
+         float distance = Vector2.Distance(transform.position, inputpos);
+         //drag has to start close to the ball 
+         if(Input.GetMouseButtonDown(0) && distance <= 0.5f) Drag();
+         if(Input.GetMouseButtonDown(0) && dragging) Dragmove();
+         if(Input.GetMouseButtonUp(0) && dragging) Release(inputpos);
+
+         
+         
+      }
+
+    // Start is called before the first frame update
+   void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+   void Update()
+    {
+        input();
+        
+    }
+}
