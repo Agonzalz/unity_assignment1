4b51cd9a56024b6fc901618f4b098c05c13bb449
Agonzalz <60372524+Agonzalz@users.noreply.github.com>
2023-09-30 21:14:08 -0700

    added extra scripts

---
+++ unity_assignment1/Assets/Scripts/Options.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Options : MonoBehaviour
+{
+    public void Return()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    
+}
